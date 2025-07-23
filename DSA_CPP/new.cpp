b3f2a81f2e5eb61852c89ed6da8e77e74083eef3
Nitish Kumar <126095096+nitish-6973@users.noreply.github.com>
2025-07-23 17:30:41 +0530

    pattern5.cpp

--- DSA_CPP/new.cpp
+++ DSA_CPP/new.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 int main()
 {
-    int n = 3;
-
-    int num = 1;
+    int n = 4;
+    char ch = 'A';
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < i + 1; j++)
         {
-            cout << num << "";
-            num++;
+            cout << ch << " ";
+            ch = ch + 1;
         }
         cout << endl;
     }
