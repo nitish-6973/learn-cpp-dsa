69706931d50d53a5c3f53a8292f7de875caec0cd
Nitish Kumar <126095096+nitish-6973@users.noreply.github.com>
2025-07-24 19:45:35 +0530

    Trianglepattern1.cpp

--- DSA_CPP/Trianglepattern1.cpp
+++ DSA_CPP/Trianglepattern1.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int n = 4;
-
     for (int i = 0; i < n; i++)
     {
-        char ch = 'A' + i;
+        int num = 1;
         for (int j = 0; j < i + 1; j++)
         {
-            cout << ch << " ";
+            cout << num << " ";
+            num = num + 1;
         }
         cout << endl;
     }
-
     return 0;
 }
