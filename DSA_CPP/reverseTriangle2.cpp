9b6ebd0bf6db8d4c5135d34eda0ec139428b3cf3
Nitish Kumar <126095096+nitish-6973@users.noreply.github.com>
2025-07-25 10:21:12 +0530

    reverseTriangle2.cpp

--- DSA_CPP/reverseTriangle2.cpp
+++ DSA_CPP/reverseTriangle2.cpp
@@ -3,14 +3,13 @@ using namespace std;
 int main()
 {
     int n = 4;
-
     for (int i = 0; i < n; i++)
     {
-
-        for (int j = i + 1; j >= 1; j--)
+        for (int j = i; j >= 0; j--)
         {
 
-            cout << j << " ";
+            char ch = 'A' + j;
+            cout << ch << " ";
         }
         cout << endl;
     }
